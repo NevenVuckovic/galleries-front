6840443bc8ee289c89cfa66643400c5932fc52dd
NevenVuckovic <nvn.vckvc@gmail.com>
2021-02-25 14:43:24 +0100

    login/register - incomplete

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav-bar />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from "./layout/NavBar.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    NavBar
   }
-}
+};
 </script>
 
 <style>
---
+++ src/layout/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="mb-3">
+    <h1>Gallery</h1>
+
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand class="navbar-brand">All Galleries</b-navbar-brand>
+      <b-navbar-brand class="navbar-brand">My Galleries</b-navbar-brand>
+      <b-navbar-brand class="navbar-brand">Create New Gallery</b-navbar-brand>
+      <b-navbar-brand class="navbar-brand">
+        <router-link to="/register">Register</router-link>
+      </b-navbar-brand>
+      <b-navbar-brand class="navbar-brand">
+        <router-link to="/login">Login</router-link>
+      </b-navbar-brand>
+      <b-navbar-brand class="navbar-brand">Logout</b-navbar-brand>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+h1 {
+  text-decoration: underline;
+  color: rgb(255, 255, 255);
+  background-color: rgb(23, 184, 117);
+  margin: auto;
+  text-align: center;
+}
+</style>
